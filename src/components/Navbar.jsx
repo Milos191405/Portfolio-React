f388965eb6dea715759d67485c55a34f5577f254
Milos Mirkovic <milos.mirkovic7@gmail.com>
2024-06-26 22:58:20 +0200

    removing border lines

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
   const closeMenu = () => setNav(false);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 border-b-2 border-red-500">
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo" className="w-[50px]" />
       </div>
